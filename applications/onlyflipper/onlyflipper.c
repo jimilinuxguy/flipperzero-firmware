d84cd27888a3cd65391e5f522341af3d2e657103
MB <101580720+MuddledBox@users.noreply.github.com>
2022-03-16 19:45:56 -0500

    Image enhancements

--- applications/onlyflipper/onlyflipper.c
+++ applications/onlyflipper/onlyflipper.c
@@ -30,7 +30,7 @@ static DialogMessageButton product_screen(DialogsApp* dialogs, DialogMessage* me
 static DialogMessageButton icon1_screen(DialogsApp* dialogs, DialogMessage* message) {
     DialogMessageButton result;
 
-    dialog_message_set_icon(message, &I_OF1_119x50, 6, 0);
+    dialog_message_set_icon(message, &I_OF2_119x50, 6, 0);
     result = dialog_message_show(dialogs, message);
     dialog_message_set_icon(message, NULL, 0, 0);
 
